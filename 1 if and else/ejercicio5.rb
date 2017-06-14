4032c71daabd1cbab8359d273851bbde6740d4f3
gazziandres <gazzi.andres@gmail.com>
2017-06-13 21:57:23 -0400

    if

---
+++ 1 if and else/ejercicio5.rb
@@ -0,0 +1,14 @@
+# Utiliza álgebra booleana para hacer un refactoring de este código
+# Para verificar la respuestas, puedes variar los valores de a y b.
+
+a = true
+b = true
+
+if a == true && b == true
+ puts 'Lograste A y B!'
+ elsif if a == true 
+ puts 'Lograste A! Pero no B!'
+else
+  puts 'No lograste A ni B!'
+ end
+end
