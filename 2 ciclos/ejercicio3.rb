1f8698660588dfd185442ec821554c36f0e2c6cb
gazziandres <gazzi.andres@gmail.com>
2017-06-13 21:58:00 -0400

    ciclos

---
+++ 2 ciclos/ejercicio3.rb
@@ -0,0 +1,18 @@
+# Mostrar todos los divisores del número 990 con:
+
+#while, for, times.
+
+for i in 1..990
+	puts i if (990 % i).zero?
+end
+
+i=0
+990.times do 
+	i =i+1
+	puts i if (990 % i).zero?
+end
+
+while i <= 990
+	puts i if (990 % i).zero?
+	i += 1
+end
